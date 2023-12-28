8b15260da9fe3ca50464cc95e02dfa3e0a27d11d
d33fur <d33fur@gmail.com>
2023-12-28 03:43:11 +0300

    fixes

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE stock_exchange
+CREATE DATABASE stock_exchange;
 
-\c stock_exchange
+\c stock_exchange;
 
-CREATE TABLE users (
+CREATE TABLE public.users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     second_name VARCHAR(255) NOT NULL,
@@ -11,24 +11,26 @@ CREATE TABLE users (
     login VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
-CREATE TYPE valid_deal_type AS ENUM ('buy', 'sell');
-CREATE TABLE deals (
+
+CREATE TYPE public.valid_deal_type AS ENUM ('buy', 'sell');
+
+CREATE TABLE public.deals (
     id SERIAL PRIMARY KEY,
     from_user_id INTEGER NOT NULL,
     to_user_id INTEGER,
-    deal_type valid_deal_type,
+    deal_type public.valid_deal_type,
     amount INTEGER NOT NULL,
     exchange_rate INTEGER NOT NULL,
     deal_status VARCHAR(255) NOT NULL,
     time_start TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    time_end TIMESTAMP,
+    time_end TIMESTAMP
 );
 
-INSERT INTO users (first_name, second_name, balance_USD, balance_RUB, login, password)
-VALUES 
-    ('John', 'Doe', 1000, 50000, 'john_doe', 'password123'),
-    ('Alice', 'Smith', 500, 25000, 'alice_smith', 'securepass');
+-- INSERT INTO public.users (first_name, second_name, balance_USD, balance_RUB, login, password)
+-- VALUES 
+--     ('John', 'Doe', 1000, 50000, 'john_doe', 'password123'),
+--     ('Alice', 'Smith', 500, 25000, 'alice_smith', 'securepass');
 
-INSERT INTO deals (from_user_id, to_user_id, deal_type, amount, exchange_rate, deal_status)
-VALUES
-    (1, 2, 'buy', 10, 75, 'completed');
+-- INSERT INTO public.deals (from_user_id, to_user_id, deal_type, amount, exchange_rate, deal_status)
+-- VALUES
+--     (1, 2, 'buy', 10, 75, 'completed');
